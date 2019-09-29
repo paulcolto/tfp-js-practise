aa199ea0ca75b7dc142964c4189a4ba6566e644d
Paul Coltofeanu <paulcolto@gmail.com>
2019-09-29 15:26:17 +0200

    files

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,26 @@
-console.log('It works');
+/* eslint-disable */
+
+const headingText = document.querySelector('#headingText');
+const changeHeadingText = () => {
+  headingText.style.color = 'white';
+  headingText.style.backgroundColor = 'black';
+};
+
+
+const infoText = document.querySelector('#infoText');
+const showText = () => {
+  infoText.style.display = 'block';
+};
+
+const hideText = () => {
+    infoText.style.display = 'none';
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  headingText.addEventListener('mouseover', changeHeadingText);
+
+  const imgIn = document.querySelector('#img')
+  imgIn.addEventListener('mouseenter', showText);
+  const imgOut = document.querySelector('#img')
+  imgOut.addEventListener('mouseleave', hideText);
+});
